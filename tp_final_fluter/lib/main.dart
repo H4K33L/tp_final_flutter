736ac01913157f49e9b26e3129223089b3537e9d
Luky-probably-not <jb.bodusseau@gmail.com>
2026-07-06 08:39:01 +0200

    pkg update

--- tp_final_fluter/lib/main.dart
+++ tp_final_fluter/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:tp_final_fluter/firebase_options.dart';
 
-
-await Firebase.initializeApp(
-  options:DefaultFirebaseOptions.currentPlatform,
-)
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options:DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
